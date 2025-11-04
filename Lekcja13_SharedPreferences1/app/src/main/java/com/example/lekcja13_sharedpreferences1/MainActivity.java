302248716c2198b8b4a780ce80b6de6980b25aaa
Jakub <jknowak.06@gmail.com>
2025-11-04 20:40:58 +0100

    prefs1

---
+++ Lekcja13_SharedPreferences1/app/src/main/java/com/example/lekcja13_sharedpreferences1/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.lekcja13_sharedpreferences1;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String PREFS_NAME = "Lekcja13_SharedPreferences1";
+    private static final String DATA_KEY = "UserInputData";
+
+    private EditText dataInput;
+    private Button saveButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        dataInput = findViewById(R.id.dataInput);
+        saveButton = findViewById(R.id.saveButton);
+
+        SharedPreferences prefs = getSharedPreferences(PREFS_NAME, MODE_PRIVATE);
+        String savedData = prefs.getString(DATA_KEY, "");
+        dataInput.setText(savedData);
+
+        saveButton.setOnClickListener(v -> {
+            SharedPreferences.Editor editor = prefs.edit();
+            editor.putString(DATA_KEY, dataInput.getText().toString());
+            editor.apply();
+            Toast.makeText(this, "Dane zapisane!", Toast.LENGTH_SHORT).show();
+        });
+    }
+}
